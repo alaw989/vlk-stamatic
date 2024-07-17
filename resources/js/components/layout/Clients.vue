cfda83ebe49295f734b6e874df7bd5dec3443a7c
Austin Law <alaw989@gmail.com>
2024-07-17 13:14:32 -0400

    turned slider back on

--- resources/js/components/layout/Clients.vue
+++ resources/js/components/layout/Clients.vue
@@ -6,7 +6,7 @@
                 <div class="flex justify-center flex-col items-center text-center w-full max-w-[90%] md:max-w-[75%]">
                     <h1 class="mb-4 lg:mb-6 text-2xl md:text-4xl font-bold text-black m-0"
                         :class="route.path !== '/' ? 'text-white' : 'text-black'">{{ clientsHeading }}</h1>
-                    <carousel :breakpoints="breakpoints" :wrap-around="true"  :items-to-show="2">
+                    <carousel :breakpoints="breakpoints" :wrap-around="true"  :autoplay="3000" :items-to-show="2">
                         <slide v-for="item in clients" :key="item.id">
                             <router-link target="_blank" :to="item.client_link ? item.client_link : ''">
                                 <div class="mx-4">
